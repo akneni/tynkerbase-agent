5d1971057f547bdc1d99cc20e13eae6a5eee6383
Anish Kanthamneni <109305222+akneni@users.noreply.github.com>
2024-06-30 02:36:19 -0400

    Add installation files

---
+++ installation/install.py
@@ -0,0 +1,15 @@
+import subprocess
+import shutil
+import os
+
+tyb_path = "/usr/share/tynkerbase-agent"
+if os.path.exists(tyb_path):
+    shutil.rmtree(tyb_path)
+if not os.path.exists(tyb_path):
+    os.makedirs(tyb_path)
+
+child = subprocess.run("git clone https://github.com/akneni/tynkerbase-agent.git /usr/share/tynkerbase-agent".split())
+
+if os.path.exists('/usr/bin/tyb_agent'):
+    os.remove('/usr/bin/tyb_agent')
+os.symlink('/usr/share/tynkerbase-agent/target/release/tynkerbase-agent', '/usr/bin/tyb_agent')
