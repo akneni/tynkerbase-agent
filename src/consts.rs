2a9f1dc60ff28ec1036949a23259e885320abeb9
Anish Kanthamneni <109305222+akneni@users.noreply.github.com>
2024-07-10 19:49:24 -0400

    minor changes

--- src/consts.rs
+++ src/consts.rs
@@ -2,12 +2,11 @@ const fn get_proj_path() -> &'static str {
     // Allows developers to build and test in their local project directory
     // rather than directly in /usr/share
 
-    #[allow(unused_assignments)]
-    let mut root_dir = "/usr/share/tynkerbase-agent";
     #[cfg(debug_assertions)] {
-        root_dir = ".";
+        return ".";
     }
-    root_dir
+    #[allow(unreachable_code)]
+    "/usr/share/tynkerbase-agent"
 }
 
 pub const AGENT_ROOTDIR_PATH: &str = get_proj_path();
