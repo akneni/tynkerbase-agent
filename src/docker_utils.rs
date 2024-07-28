d90bed71c8ed44a7924f1cd8d68d787c8508b76a
Anish Kanthamneni <109305222+akneni@users.noreply.github.com>
2024-07-28 19:45:55 -0400

    minor changes

--- src/docker_utils.rs
+++ src/docker_utils.rs
@@ -223,7 +223,9 @@ pub async fn delete_container(container_name: impl AsRef<str>) -> Result<()>{
     if !output.status.success() {
         let err = String::from_utf8(output.stderr)
             .unwrap_or("Unable to extract stderr".to_string());
-        return Err(anyhow!("Failed to delete container `{}`:\n{}", container_name, err));
+        let err = anyhow!("Failed to delete container `{}`:\n{}", container_name, err);
+        println!("{:#?}", err);
+        return Err(err);
     }
 
     Ok(())
